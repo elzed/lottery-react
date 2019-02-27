7ba3404b41cf409e88e7328905f44c4a54a8c6e9
elzed <elzed@users.noreply.github.com>
2019-02-27 14:09:41 -0800

    Assign MetaMask values to new Web3 instance

---
+++ src/web3.js
@@ -0,0 +1,8 @@
+import Web3 from 'web3'; // constructor
+
+// A new instance of Web3
+// currentProvider has access to Rinkeby with keys, etc. courtesy MetaMask config
+// currentProvider values are placed in our newly constructed web3
+const web3 = new Web3(window.web3.currentProvider);
+
+export default web3;
