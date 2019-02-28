a0ec4fc5e11e4ebcc6d4480cfda8ebb83cd41a5d
elzed <elzed@users.noreply.github.com>
2019-02-28 09:44:09 -0800

    Create a new local contract instance using web3
    Set up local copy of the contract that refers to the correct address on
    the Rinkeby test network. The ABI and Address are returned data from
    running the deploy.js script from the solidity-lottery project.

---
+++ src/lottery.js
@@ -0,0 +1,50 @@
+import web3 from './web3';
+
+// Address and ABI returned from deploy.js
+// within the solidity-lottery project
+const address = '0xC9571432b78BE5dEcf4c05E81f6ba4a281e92C58';
+const abi = [{
+    "constant": true,
+    "inputs": [],
+    "name": "manager",
+    "outputs": [{"name": "", "type": "address"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [{"name": "", "type": "address[]"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{"name": "", "type": "uint256"}],
+    "name": "players",
+    "outputs": [{"name": "", "type": "address"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {"inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor"}];
+
+// Export a complete copy of contract to be used
+// within the React app
+export default new web3.eth.Contract(abi, address);
